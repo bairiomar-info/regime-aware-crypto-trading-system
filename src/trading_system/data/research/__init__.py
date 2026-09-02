f5530b87f7d3fd930fb2913476d53261abf4173c
bairiomar-info <bairiomar36@gmail.com>
2026-09-02 13:53:20 +0100

    Export readiness and liquidity interfaces

--- src/trading_system/data/research/__init__.py
+++ src/trading_system/data/research/__init__.py
@@ -1,5 +1,6 @@
 """Point-in-time research universe and dataset contracts."""
 
+from .liquidity import rolling_quote_volume
 from .manifest import ResearchDatasetManifest
 from .membership import MembershipEvent, build_membership_intervals, membership_at
 from .models import (
@@ -9,6 +10,7 @@ from .models import (
     PointInTimeMembership,
 )
 from .policy import UniversePolicy
+from .readiness import ReadinessDecision, ReadinessState, assess_readiness
 from .universe import evaluate_eligibility
 
 __all__ = [
@@ -17,9 +19,13 @@ __all__ = [
     "MembershipEvent",
     "MembershipStatus",
     "PointInTimeMembership",
+    "ReadinessDecision",
+    "ReadinessState",
     "ResearchDatasetManifest",
     "UniversePolicy",
+    "assess_readiness",
     "build_membership_intervals",
     "evaluate_eligibility",
     "membership_at",
+    "rolling_quote_volume",
 ]
