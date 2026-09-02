3d73701d528fa4e66d934c2f8e95b0948408463f
bairiomar-info <bairiomar36@gmail.com>
2026-09-02 12:15:38 +0100

    Export asset identity and lineage models

--- src/trading_system/data/models/__init__.py
+++ src/trading_system/data/models/__init__.py
@@ -1,7 +1,19 @@
 """Canonical market-data domain models."""
 
+from .asset import Asset, AssetLifecycle
 from .candle import Candle
 from .instrument import Instrument, MarketType
+from .lineage import AssetLineageEvent, LineageConfidence, LineageEventType
 from .timeframe import Timeframe
 
-__all__ = ["Candle", "Instrument", "MarketType", "Timeframe"]
+__all__ = [
+    "Asset",
+    "AssetLifecycle",
+    "AssetLineageEvent",
+    "Candle",
+    "Instrument",
+    "LineageConfidence",
+    "LineageEventType",
+    "MarketType",
+    "Timeframe",
+]
