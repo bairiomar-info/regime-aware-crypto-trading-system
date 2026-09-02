747ec69e41a77e7fec2f0686b7280bb0479f5a62
bairiomar-info <bairiomar36@gmail.com>
2026-09-02 14:43:04 +0100

    Export causal regime classifier API

--- src/trading_system/regime/__init__.py
+++ src/trading_system/regime/__init__.py
@@ -1,11 +1,29 @@
 """V1 multidimensional market-regime primitives."""
 
+from .classifier import (
+    Dimension,
+    DimensionClassification,
+    DimensionConfig,
+    DimensionTracker,
+    RegimeClassificationResult,
+    RegimeClassifierConfig,
+    RegimeClassifierState,
+    classify_market_state,
+)
 from .hysteresis import HysteresisConfig, HysteresisResult, HysteresisState, update_hysteresis
 from .models import LevelState, MarketState, Transition, TrendState
 from .state import evidence_confidence, transition_for
 from .thresholds import classify_three_level, classify_trend, empirical_quantile
 
 __all__ = [
+    "Dimension",
+    "DimensionClassification",
+    "DimensionConfig",
+    "DimensionTracker",
+    "RegimeClassificationResult",
+    "RegimeClassifierConfig",
+    "RegimeClassifierState",
+    "classify_market_state",
     "HysteresisConfig",
     "HysteresisResult",
     "HysteresisState",
