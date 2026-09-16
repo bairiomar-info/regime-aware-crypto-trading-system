6d618a6070413498d9f0552f671210231a8e907a
bairiomar-info <bairiomar36@gmail.com>
2026-09-16 14:19:51 +0100

    feat: expose backtest result statistics

--- src/trading_system/backtest/__init__.py
+++ src/trading_system/backtest/__init__.py
@@ -1,13 +1,18 @@
 """Backtesting public API."""
 
 from .engine import BacktestConfig, BacktestState, MarketBar, execute_signal
+from .results import BacktestResult, EquityPoint, calculate_max_drawdown, calculate_total_return
 from .walk_forward import WalkForwardWindow, make_walk_forward_windows
 
 __all__ = [
     "BacktestConfig",
+    "BacktestResult",
     "BacktestState",
+    "EquityPoint",
     "MarketBar",
     "WalkForwardWindow",
+    "calculate_max_drawdown",
+    "calculate_total_return",
     "execute_signal",
     "make_walk_forward_windows",
 ]
