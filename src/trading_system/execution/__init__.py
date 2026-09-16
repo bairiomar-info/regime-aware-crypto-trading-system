76c0f69942e152bc4a2cae292dfff8c2816f5f92
bairiomar-info <bairiomar36@gmail.com>
2026-09-16 14:55:40 +0100

    feat: expose paper execution API

--- src/trading_system/execution/__init__.py
+++ src/trading_system/execution/__init__.py
@@ -2,6 +2,16 @@
 
 from .broker import Broker, BrokerFill
 from .gate import PreTradeConfig, validate_pre_trade
+from .paper import PaperExecutionResult, PaperFill, execute_paper_order
 from .service import execute_order
 
-__all__ = ["Broker", "BrokerFill", "PreTradeConfig", "execute_order", "validate_pre_trade"]
+__all__ = [
+    "Broker",
+    "BrokerFill",
+    "PaperExecutionResult",
+    "PaperFill",
+    "PreTradeConfig",
+    "execute_order",
+    "execute_paper_order",
+    "validate_pre_trade",
+]
