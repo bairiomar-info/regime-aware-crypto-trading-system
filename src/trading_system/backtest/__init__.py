39fe1e41394f6fce8defe6c1706909003141b0bb
bairiomar-info <bairiomar36@gmail.com>
2026-09-16 14:24:16 +0100

    feat: expose OOS evaluation API

--- src/trading_system/backtest/__init__.py
+++ src/trading_system/backtest/__init__.py
@@ -1,6 +1,7 @@
 """Backtesting public API."""
 
 from .engine import BacktestConfig, BacktestState, MarketBar, execute_signal
+from .evaluation import OOSSummary, OOSWindowResult, summarize_oos
 from .results import BacktestResult, EquityPoint, calculate_max_drawdown, calculate_total_return
 from .walk_forward import WalkForwardWindow, make_walk_forward_windows
 
@@ -10,9 +11,12 @@ __all__ = [
     "BacktestState",
     "EquityPoint",
     "MarketBar",
+    "OOSSummary",
+    "OOSWindowResult",
     "WalkForwardWindow",
     "calculate_max_drawdown",
     "calculate_total_return",
     "execute_signal",
     "make_walk_forward_windows",
+    "summarize_oos",
 ]
